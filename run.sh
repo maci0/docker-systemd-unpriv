54c3300d578569b91092a61e0422878e0bcfb87f
maci <maci@satgnu.net>
2014-07-23 19:46:13 +0200

    same but cleaner

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 TMPDIR=$(mktemp -d)
-mkdir -p ${TMPDIR}/run/lock
 NAME=docker-systemd-$(echo ${TMPDIR}|cut -d\. -f2)
-sudo docker run --name=${NAME} --rm -t -i -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /${TMPDIR}/run:/run maci/systemd /usr/lib/systemd/systemd
+sudo docker run --name=${NAME} --rm -t -i -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+-v /${TMPDIR}/run:/run -v /${TMPDIR}/run/lock:/run/lock maci/systemd /usr/lib/systemd/systemd
