5b04be32a52e3c2427f05bb8bebd5be6feef5cdb
maci <maci@satgnu.net>
2014-07-23 19:45:19 +0200

    some packages expect /run/lock

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 TMPDIR=$(mktemp -d)
+mkdir -p ${TMPDIR}/run/lock
 NAME=docker-systemd-$(echo ${TMPDIR}|cut -d\. -f2)
 sudo docker run --name=${NAME} --rm -t -i -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /${TMPDIR}/run:/run maci/systemd /usr/lib/systemd/systemd
