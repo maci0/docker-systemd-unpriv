39d48a05bd15cd81258af5e5c09789ebf42e16b2
Marcel Wysocki <maci.stgn@gmail.com>
2015-07-22 11:32:44 +0200

    Fix typo in run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 sudo date
 CONTAINER=$(sudo docker run -d -v /sys/fs/cgroup:/sys/fs/cgroup:ro maci/systemd /usr/lib/systemd/systemd)
 echo ${CONTAINER}
-echo "to enter docker container run: 
+echo "To enter docker container run: 
         docker exec -t -i ${CONTAINER} /bin/bash"
